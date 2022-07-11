812edc8af31e6481e256f61361f830d8c643a3c0
Okinaws <kiriton02@mail.ru>
2022-07-11 23:38:13 +0300

    added greeting

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const time = document.querySelector(".time");
 const date = document.querySelector(".date");
+const greeting = document.querySelector(".greeting");
+const name = document.querySelector(".name");
 
 
 function showTime() {
@@ -17,3 +19,33 @@ function showDate() {
     const options = {month: 'long', day: 'numeric'};
     date.textContent = `${days[currentDate.getDay()]}, ${currentDate.toLocaleDateString('en-US', options)}`;
 }
+
+function showGreeting() {
+    let hour = new Date().getHours();
+    let timeOfDay = "Night";
+    if (hour > 5 && hour <= 12) {
+        timeOfDay = "Morning";
+    }
+    else if (hour > 12 && hour <= 18) {
+        timeOfDay = "Day";
+    }
+    else if (hour > 18 && hour <= 23) {
+        timeOfDay = "Evening";
+    }
+    greeting.textContent = `Good ${timeOfDay}, `;
+}
+
+showGreeting();
+
+function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+}
+
+function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+}
+
+window.addEventListener('load', getLocalStorage)
+window.addEventListener('beforeunload', setLocalStorage)
